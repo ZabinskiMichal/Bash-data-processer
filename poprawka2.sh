d22ff4c1915c7541ce504e1be89485627da00071
Michal Zabinski <zabinski@student.agh.edu.pl>
2022-08-17 15:08:57 +0200

    wersjaPierwsza

---
+++ poprawka2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#0 START
+
+#tutaj trzeba zamiast %s dac %N ale niestety u mnie na kompie to nie działa
+startTime=`date +%s`
+
+if [ $# == 0 ]
+then
+echo "brak argumentow, sprobuj: $0 <KAT_BAZOWY> <nazwa_pliku_danych1> <nazwa_pliku_danych2> <nazwa_pliku_danychN>" 1>&2
+exit 1
+elif [ $# == 1 ]
+then
+echo "podano tylko jeden argument, sprobuj: $0 <KAT_BAZOWY> <nazwa_pliku_danych1> <nazwa_pliku_danych2> <nazwa_pliku_danychN>" 1>&2
+exit 2
+else
+
+fi
+
+echo $()
+
+
+stopTime=`date +%s`
+#miktosekunda ma dokalosc 6 miejsc po przecinku
+#-n po echo sprawia ze nie ma znaku konca lini, i nastepne echo bedzie w tej samej linijce
+echo -n $(ps -a -o pid,ppid | grep $(ps -a | grep poprawka2.sh | cut -d " " -f 1 | head -n 1) | head -n 1) >> out.log
+echo -n " " >> out.log
+#bc to basic kalkulator, pozwala na wykonywanie obliczen z przecinkiem
+echo "scale=2;$stopTime-$startTime" | bc >> out.log
+
+#trzeba jeszcze dodac czwart elemtn czyli wiersz poeleenia uruchumienia ale nwm o co z tym chodzi
+
+
+
